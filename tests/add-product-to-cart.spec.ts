df76868460b7a1bfa4a42ef8f10fd3a657708cf4
Trang Nguyen Thu <trangnguyent5@nashtechglobal.com>
2026-02-06 16:28:08 +0700

    add new file handle both case add one and add multiple products to cart

---
+++ tests/add-product-to-cart.spec.ts
@@ -0,0 +1,61 @@
+import { test, expect } from '../base/base-test'
+import { users } from '../data/users'
+import { products } from '../data/products'
+
+test.describe('Cart - add products to cart', () => {
+
+  test.beforeEach(async ({ loginPage, inventoryPage }) => {
+    await loginPage.login(
+      users.standard.username,
+      users.standard.password
+    )
+    await inventoryPage.verifyOnInventoryPage()
+  })
+
+  test('User can add one product to cart', async ({ inventoryPage, cartPage }) => {
+
+    const product = products.backpack
+
+    await test.step('User adds a product to cart', async () => {
+      await inventoryPage.addProductToCart(product)
+    })
+
+    await test.step('User opens cart page', async () => {
+      await inventoryPage.openCart()
+    })
+
+    await test.step('User should see the product in the cart', async () => {
+      const item = await cartPage.getItemByName(product)
+      await expect(item).toBeVisible()
+    })
+
+  })
+
+  test('User can add multiple products to cart', async ({ inventoryPage, cartPage }) => {
+
+    const productsToAdd = [
+      products.backpack,
+      products.light,
+      products.jacket
+    ]
+
+    await test.step('User adds multiple products to cart', async () => {
+      for (const product of productsToAdd) {
+        await inventoryPage.addProductToCart(product)
+      }
+    })
+
+    await test.step('User opens cart page', async () => {
+      await inventoryPage.openCart()
+    })
+
+    await test.step('User should see all added products in the cart', async () => {
+      for (const product of productsToAdd) {
+        const item = await cartPage.getItemByName(product)
+        await expect(item).toBeVisible()
+      }
+    })
+
+  })
+
+})
