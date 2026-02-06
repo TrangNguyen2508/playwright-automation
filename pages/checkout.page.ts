cd67b13263fbd7355483a12b85989c1bebaebd5b
Trang Nguyen Thu <trangnguyent5@nashtechglobal.com>
2026-02-06 09:54:09 +0700

    resolve conflict

--- pages/checkout.page.ts
+++ pages/checkout.page.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type{Page, Locator} from '@playwright/test'
 export class CheckoutPage{
     readonly page: Page
@@ -29,36 +28,4 @@ export class CheckoutPage{
     async finishCheckOut(){
         await this.finishButton.click()
     }
-=======
-import type{Page, Locator} from '@playwright/test'
-export class CheckoutPage{
-    readonly page: Page
-    readonly firstNameInput: Locator
-    readonly lastNameInpur: Locator
-    readonly postalCodeInput: Locator
-    readonly continueButton: Locator
-    readonly finishButton: Locator
-    readonly successMessage: Locator
-
-    constructor (page: Page){
-        this.page=page
-        this.firstNameInput = page.locator('[data-test="firstName"]')
-        this.lastNameInpur = page.locator('[data-test="lastName"]')
-        this.postalCodeInput = page.locator('[data-test="postalCode"]')
-        this.continueButton = page.locator('[data-test="continue"]')
-        this.finishButton = page.locator('[data-test="finish"]')
-        this.successMessage = page.locator('[data-test="complete-header"]')
-    }
-
-    async fillInformation(firstName: string, lastName: string, zipCode: string){
-        await this.firstNameInput.fill(firstName)
-        await this.lastNameInpur.fill(lastName)
-        await this.postalCodeInput.fill(zipCode)
-        await this.continueButton.click()
-    }
-    
-    async finishCheckOut(){
-        await this.finishButton.click()
-    }
->>>>>>> 0b1fee05cd924de33373d95d48354ead14eb2ea1
 }
